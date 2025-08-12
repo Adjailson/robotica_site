4f2a6c9ea76636c1fef30f84c90ea8f251fa3a29
Liandro-B <liandroferreira314@gmail.com>
2025-08-12 19:18:59 -0300

    Alteração das routes, o index e lógica do código
    Alterei as pastas e coloquei novos arquivos para ficar semelhante com um site com estrutura flask. Adicionei a index(incompleta) e fiz uma estrutura geral do index para criação de outras páginas. Removi o arquivo app para fora da pasta para organizar todos elementos e arquivos para conseguir rodar o site fora da propria pasta do site

--- routes.py
+++ routes.py
@@ -1,1 +1,24 @@
-# Podemos usar apenas no final do projeto
+# Podemos usar apenas no final do projeto - Liandro
+from flask import Flask, render_template, url_for
+from robotica_site import app
+
+# Coloquei rotas para todos os arquivos html como teste. Ainda irei alterar essas rotas - Liandro
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
+
+@app.route('/eventos')
+def eventos():
+    return render_template('eventos.html')
+
+@app.route('/galeria')
+def galeria():
+    return render_template('galeria.html')
+
+@app.route('/espaço-maker')
+def espaco():
+    return render_template('espaco_maker.html')
