12f183f09bfd54451bdbb59fe19e04a959ab8218
Liandro-B <liandroferreira314@gmail.com>
2025-09-05 11:57:49 -0300

    Fazendo o banco de dados
    Criando as tabelas e colunas para implantar no banco de dados. Contudo ainda não criei o arquivo do banco de dados

--- robotica_site/models.py
+++ robotica_site/models.py
@@ -4,10 +4,26 @@ from flask_login import UserMixin
 from datetime import datetime
 
 class Noticias(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    descricao = database.Column(database.String, nullable = False)
+    titulo = database.Column(database.String, nullable = False)
+    link = database.Column(database.String, nullable = True, default = None)
+    data_de_criacao = database.Column(database.DateTime, nullable = False, default =datetime.today())
+    imagem = database.Column(database.String, nullable = False)
 
 class Usuario(database.Model, UserMixin):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    status = database.Column(database.Integer, default = 4, nullable = False)
+    nome = database.Column(database.String, nullable = False)
+    sobrenome = database.Column(database.String, nullable = False)
+    email = database.Column(database.String, nullable = False, unique = True)
+    senha = database.Column(database.String, nullable = False)
+    imagem = database.Column(database.String, nullable = True, default = None)
 
 class Projeto(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    descricao = database.Column(database.String, nullable = False)
+    titulo = database.Column(database.String, nullable = False)
+    link = database.Column(database.String, nullable = True, default = None)
+    imagem = database.Column(database.String, nullable = False)
+    participantes = database.relationship('Usuario', backref='projeto', lazy= True)
